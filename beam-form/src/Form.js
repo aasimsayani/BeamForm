0fb8bf4ff2a41379e4be0e4c12af2265da924fb6
aasimniz95@gmail.com <aasimniz95@gmail.com>
2018-10-29 22:31:19 -0700

    Updated Packages and Form

--- beam-form/src/Form.js
+++ beam-form/src/Form.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
 import { Card, Container, Dimmer, Form, Loader, Segment } from 'semantic-ui-react';
 
-
-
 class Forms extends Component {
-
   state = {
+    data: {
     loadingText: '',
     coin: '',
+    amount: '',
     address: '',
     signature: '',
-  };
+    username: '',
+    password: '',
+    email: ''
+    }
+  }
 
-  onFormSubmit = async (event) => {
-    event.preventDefault();
+  handleChange = e => this.setState({
+    data: {...this.state.data, [e.target.name]: e.target.value }
+  })
+
+  onFormSubmit = () => {
+    const { username, email, coin, amount, address, signature, password} = this.state
 
     this.setState({
       loadingText: 'Verifying...',
-    })
+      coin: coin,
+      username: username,
+      email: email,
+      password: password,
+      signature: signature,
+      amount: amount,
+      address: address
+    });
+    console.log(this.state.data)
   }
 
-
     render() {
+      const { data } = this.state
+
       return(
+        <div>
         <Container style={{ marginTop: '7em' }}>
         <Segment basic>
           <Card centered>
@@ -32,23 +49,40 @@ class Forms extends Component {
             <Card.Content>
             <Form onSubmit ={this.onFormSubmit}>
               <Form.Field required>
+                <label>Username</label>
+                <input value={data.username} name='username' onChange={this.handleChange}/>
+              </Form.Field>
+              <Form.Field required>
+                <label>Password</label>
+                <input value={data.password} name='password' onChange={this.handleChange}/>
+              </Form.Field>
+              <Form.Field required>
+                <label>Email</label>
+                <input value={data.email} name='email'onChange={this.handleChange}/>
+              </Form.Field>
+              <Form.Field required>
                 <label>Coin</label>
-                <input value={this.state.coin}  />
+                <input value={data.coin} name='coin' onChange={this.handleChange}/>
+              </Form.Field>
+              <Form.Field required>
+                <label>Amount</label>
+                <input value={data.amount} name='amount' onChange={this.handleChange}/>
               </Form.Field>
               <Form.Field required>
                 <label>Address</label>
-                <input value={this.state.address}/>
+                <input value={data.address} name='address' onChange={this.handleChange}/>
               </Form.Field>
               <Form.Field required>
                 <label>Signature</label>
-                <input value={this.state.signature}  />
+                <input value={data.signature} name='signature' onChange={this.handleChange}/>
               </Form.Field>
-              <Form.Button primary disabled={this.state.disabled}>Submit</Form.Button>
+              <Form.Button>Submit</Form.Button>
              </Form>
                </Card.Content>
               </Card>
            </Segment>
         </Container>
+      </div>
      );
   };
 };
